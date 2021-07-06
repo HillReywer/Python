78276a3f7b6193902e9bf9959a8a6e7c1d7ee898
Andreii Kuzmin <ai.kuzmin@pm.me>
2021-07-06 16:23:20 +0300

    Days of life

---
+++ Main/week.py
@@ -0,0 +1,12 @@
+age = input("Age?")
+age_int = int(age)
+
+now = int(age)
+
+years = 100 - age_int
+month = years * 12
+weeks = years * 52
+days = years * 365
+
+message = f"You have {days} days, {weeks} weeks, and {years} years remaining"
+print(message)
